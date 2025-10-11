05accb422c3e75082cef73e6f3ae1eadd8f637b4
lncxr <lncxr@proton.me>
2025-10-11 22:51:07 +0100

    Initial commit

---
+++ incl/visualise_ascii.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+void visualise_ascii(const std::string &path, size_t start, size_t end);
---
+++ src/visualiser.cpp
@@ -0,0 +1,5 @@
+#include "../incl/visualise_ascii.h"
+
+int main() {
+  return 0;
+}
